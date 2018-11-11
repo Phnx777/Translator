984e12d9c6da5199655142400c5425f228d5e5a0
Imal <Faruk_I_G@onyx-team.com>
2018-11-11 22:34:58 +0300

    Initial Commit

---
+++ Translator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by Имал Фарук on 11/11/2018.
+//  Copyright © 2018 Имал Фарук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
